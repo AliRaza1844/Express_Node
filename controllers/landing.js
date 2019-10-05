fe47c0fb266910dddc8c60ac1f21b0a5dc428a28
AliRaza1844 <aliraza.cs15@gmail.com>
2019-10-06 04:15:04 +0500

    +1 Add post route to submit email

--- controllers/landing.js
+++ controllers/landing.js
@@ -2,3 +2,8 @@
 exports.get_landing = function(req, res, next) {
     res.render('index', { title: 'Express' });
 }
+
+exports.submit_lead = function(req, res, next) {
+    console.log('lead email', req.body.lead_email);
+    res.redirect('/');
+}
