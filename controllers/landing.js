91e970639a8506fe5b410397ff24bf9c636f1311
AliRaza1844 <aliraza.cs15@gmail.com>
2019-11-01 03:56:43 +0500

    +1 Implement a leads route to show all the submitted emails

--- controllers/landing.js
+++ controllers/landing.js
@@ -7,7 +7,13 @@ exports.submit_lead = function(req, res, next) {
     return models.Lead.create({
         email: req.body.lead_email
     }).then(lead=>{
-        res.redirect('/');
+        res.redirect('leads');
     })
     
+}
+
+exports.show_leads = function(req, res, next) {
+    models.Lead.findAll().then(leads=>{
+        res.render('index', { title: 'Express', leads: leads});
+    })
 }
